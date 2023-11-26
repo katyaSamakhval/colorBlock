a315629909a50f07d2da135e621b7f2bb02914f1
unknown <katya.samakhval@gmail.com>
2023-11-26 21:49:30 +0300

    color blocks

---
+++ colorBlocks.js
@@ -0,0 +1,8 @@
+let name = "Joe";
+    let i = 0;
+    while (i < 2) {
+        i = i + 1;
+        document.write("Happy Birthday to you.<br>");
+    }
+    document.write("Happy Birthday dear" + name + "<br>")
+    document.write("Happy Birthday to you.<br>");
